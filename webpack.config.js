5fc3dba042e064da0e7b9754b5af1605da149032
ewclide <ewclide@yandex.ru>
2018-07-28 16:40:34 +0300

    update webpack config and package json

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,25 @@ const output = "dew";
 const entry  = "./core/main.js";
 const result = "result";
 
-var min = process.argv.indexOf('-p') !== -1,
-	UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
-	path = require('path').resolve(__dirname, result);
+var path = require('path').resolve(__dirname, result);
 
 module.exports = {
 	entry: entry,
 	output: {
 		path: path,
-		filename: output + (min ? '.min.js' : '.dev.js')
+		filename: output + '.min.js',
+		publicPath: result
 	},
-	plugins: min ? [ new UglifyJsPlugin() ] : [],
 	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader?presets[]=env'
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: 'env'
 				}
 			}
-		]
+		}]
 	}
 };
