10877a2d692e01cd71c0cc435023e5fd80ebac64
ewclide <ewclide@yandex.ru>
2018-10-14 15:37:17 +0300

    update publish - replace foreach by for

--- core/functions.js
+++ core/functions.js
@@ -170,32 +170,40 @@ export function construct(Cls, args)
 
 export function publish(TheClass, fields, methods)
 {
-	var list = {};
+    var list = {};
 
     function Output()
     {
-    	var id = Math.random();
-    	list[id] = construct(TheClass, arguments);
-    	this.id = id;
+        var id = Math.random();
+        list[id] = construct(TheClass, arguments);
+        this.id = id;
     }
 
-    fields.forEach( field => {
-    	define(Output.prototype, field, {
-    		get : function(){
-    			return list[this.id][field];
-    		},
-    		set : function(value){
-    			list[this.id][field] = value;
-    		}
-    	})
-    });
+    if (fields)
+    for (var i = 0; i < fields.length; i++)
+    {
+        let field = fields[i];
 
-    methods.forEach( method => {
-    	Output.prototype[method] = function(){
-    		var obj = list[this.id];
-    		obj[method].apply(obj, arguments);
-    	}
-    });
+        Object.defineProperty(Output.prototype, field, {
+            configurable : false,
+            get : function(){
+                return list[this.id][field];
+            },
+            set : function(value){
+                list[this.id][field] = value;
+            }
+        });
+    }
+
+    if (methods)
+    for (var i = 0; i < methods.length; i++)
+    {
+        let method = methods[i];
+        Output.prototype[method] = function(){
+            var obj = list[this.id];
+            return obj[method].apply(obj, arguments);
+        }
+    }
 
     return Output;
 }
