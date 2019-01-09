15175ad7c64e0b4b7f7513a115e31bee1d5a920e
ewclide <ewclide@yandex.ru>
2019-01-09 23:05:41 +0300

    added media method to stylesheet

--- core/stylesheet.js
+++ core/stylesheet.js
@@ -1,9 +1,13 @@
-import {printErr} from './functions';
+import { printErr, camelCaseToDash } from './functions';
 
 export default class StyleSheet
 {
 	constructor() {
 		this.element = this._createElement();
+	}
+
+	get isStyleSheet() {
+		return true;
 	}
 
 	_createElement() {
@@ -20,6 +24,23 @@ export default class StyleSheet
 		}
 	}
 
+	_getLastIndex() {
+		return this.element.cssRules.length;
+	}
+
+	add() {
+		if (arguments.length > 1) {
+			const [ rule, styles ] = arguments;
+			this._add(rule, styles);
+
+		} else if (Array.isArray(arguments[0])) {
+			arguments[0].forEach((styles) => {
+				const { rule } = styles;
+				this._add(rule, style);
+			});
+		}
+	}
+
 	_add(rule, styles) {
 		if (typeof rule !== 'string') {
 			printErr(`rule "${rule}" argument must be a string`);
@@ -30,69 +51,136 @@ export default class StyleSheet
 			return;
 		}
 
-		const strStyles = this._stylesToString(styles);
-		const index = this.element.cssRules.length;
+		const strRule = StyleSheet.serialize(rule, styles);
+		const index = this._getLastIndex();
 
-		this._insert(rule, strStyles, index);
+		this.element.insertRule(strRule, index);
 
 		return index;
 	}
 
-	add() {
-		if (arguments.length > 1) {
-			const [ rule, styles ] = arguments;
-			this._add(rule, styles);
-
-		} else if (typeof arguments[0] == 'object') {
-			const styles = arguments[0];
-
-			for (rule in styles) {
-				this._add(rule, styles[rule]);
-			}
-		}
-	}
-
-	_insert(rule, styles, index) {
-		if ('insertRule' in this.element) {
-			this.element.insertRule(`${rule} {${styles}}`, index);
-		} else {
-			this.element.addRule(rule, styles, index);
-		}
-	}
-
-	addKeyFrames(name, keyFrames) {
+	keyFrames(name, keyFrames) {
 		if (!Array.isArray(keyFrames) || keyFrames.length < 2) {
-			printErr('KeyFrames must be an array with more than 2 elements');
+			printErr('keyframes must be an array with more than 1 elements');
 			return;
 		}
 
-		const index = this.element.cssRules.length;
+		const index = this._getLastIndex();
 		const keyCounts = keyFrames.length;
 		const rule = '@keyframes ' + name;
-		let styles = '';
 
+		let styles = '';
 		keyFrames.forEach((keyFrame, i) => {
 			const { offset = i / keyCounts } = keyFrame;
-			const keyStyles = this._stylesToString(keyFrame);
-			styles += `${offset * 100}% {${keyStyles}}`;
+			styles += StyleSheet.serialize((offset * 100) + '%', keyFrame);
 		});
 
-		this._insert(rule, styles, index);
+		this.element.insertRule(`${rule} {${styles}}`, index);
+
+		return index;
+	}
+
+	media(request, arrStyles) {
+		if (typeof request !== 'object') {
+			printErr('media request must be an object');
+			return;
+		}
+
+		const mediaRule = this._buildMediaRule(request);
+		const index = this._getLastIndex();
+
+		this.element.insertRule(`${mediaRule} {}`, index);
+
+		const media = new Media(this.element, index);
+
+		if (Array.isArray(arrStyles)) {
+			arrStyles.forEach((styles) => {
+				const { rule } = styles;
+				media.add(rule, styles);
+			});
+		}
+
+		return media;
+	}
+
+	_buildMediaRule(request) {
+		const { only } = request;
+
+		let rule = '@media';
+		let first = true;
+
+		if (only) {
+			rule += ' only';
+			request.only = null;
+		}
+
+		for (let name in request) {
+			const value = request[name];
+			if (!value) continue;
+
+			const option = camelCaseToDash(name);
+
+			rule += first ? ' ' : ' and ';
+
+			if (typeof value == 'boolean') {
+				rule += option;
+			} else if (typeof value == 'number') {
+				rule += `(${option}:${value}px)`;
+			} else {
+				rule += `(${option}:${value})`;
+			}
+
+			first = false;
+		}
+
+		return rule;
+	}
+
+	remove(rule) {
+		const index = rule.isMedia ? rule.index : rule;
+		this.element.deleteRule(index);
+	}
+
+	static serialize(rule, styles) {
+		let str = rule + '{';
+
+		for (const name in styles) {
+			if (name === 'rule') continue;
+			str += `${name}:${styles[name]};`;
+		}
+
+		return str + '}';
+	}
+}
+
+class Media
+{
+	constructor(parent, index) {
+		this._media = parent.cssRules[index];
+		this._rules = this._media.cssRules;
+		this.index = index;
+	}
+
+	get isMedia() {
+		return true;
+	}
+
+	add(rule, styles) {
+		const index = this._rules.length;
+		const strRule = StyleSheet.serialize(rule, styles);
+
+		this._media.insertRule(strRule, index);
 
 		return index;
 	}
 
 	remove(index) {
-		this.element.deleteRule(index);
+		this._media.deleteRule(index);
 	}
 
-	_stylesToString(styles) {
-		let result = '';
-
-		for (const name in styles) {
-			result += `${name}:${styles[name]};`;
+	clear() {
+		for (let i = 0; i < this._rules.length; i++) {
+			this._media.deleteRule(i)
 		}
-
-		return result;
 	}
 }
--- test/html/stylesheets/index.js
+++ test/html/stylesheets/index.js
@@ -1,11 +1,35 @@
 const cascad = $html.cascad();
-cascad.addKeyFrames('fade', [
+
+const fade = cascad.keyFrames('fade', [
     { opacity: 0 },
     { opacity: 1 }
 ]);
+
 cascad.add('.test', {
-    animation: 'fade 2s'
+    animation: 'fade 5s'
 });
+
+const media = cascad.media({
+    screen: true,
+    maxWidth: 768,
+    minWidth: 320
+},[
+    {
+        rule: '.test',
+        animation: 'fade 0.5s'
+    }
+]);
+
+// const mTest = media.add('.test', {
+//     animation: 'fade 0.5s'
+// });
+// media.remove(mTest);
+// media.clear();
+
+// cascad.remove(fade);
+// cascad.remove(media);
 
 const h1 = $html.create('h1', 'test').text('Test');
 $html.body.append(h1);
+
+console.log(cascad, media)
