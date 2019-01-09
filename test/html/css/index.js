f8fd274a113b639aed395d48bff42cb61406b83f
ewclide <ewclide@yandex.ru>
2019-01-09 21:39:58 +0300

    updated css test

--- test/html/css/index.js
+++ test/html/css/index.js
@@ -1,11 +1,15 @@
-let h1 = $html.select("h1").style("color", "red");
-$html.select(".text-block")
-.style({
-	display : "inline-block",
-	background : "#ebebeb",
-	color : "red",
-	padding : "20px",
-	marginTop : "10px"
+let h1 = $html.select('h1').style('color', 'red');
+$html.select('.text-block')
+.styles({
+	display : 'inline-block',
+	background : '#ebebeb',
+	color : 'red',
+	padding : '20px',
+	marginTop : '10px'
 })
+.styles('border', {
+	bottom: '2px solid red',
+	top: '1px solid blue'
+});
 
-log(h1.style("color"))
+log(h1.style('color'))
