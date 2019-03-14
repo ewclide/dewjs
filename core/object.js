a6fcb5941fc473ab433ec84954fc522bb5092aa3
m_ustinov <m.ustinov@webgears.app>
2019-03-14 19:01:28 +0300

    added search method to object

--- core/object.js
+++ core/object.js
@@ -120,6 +120,52 @@ export function init(target, values, settings, common = { errors : true }) {
     return initer.errors.length ? (common.errors && printErr(initer.errors), false) : true;
 }
 
+function _searchInObject(root, key, value, chld, all, depth) {
+    if (!depth) return;
+
+    const result = [];
+    const field = root[key];
+
+    if (value === field) {
+        result.push(root);
+        if (!all) return result;
+    }
+
+    const children = root[chld];
+
+    if (Array.isArray(children)) {
+        depth--;
+        for (const child of children) {
+            const inside = _searchInObject(child, key, value, chld, all, depth);
+            if (inside && inside.length) {
+                result.push(...inside);
+                if (!all) return result;
+            }
+        }
+    } else if (typeof children == 'object') {
+        depth--;
+        const found = _searchInObject(children, key, value, chld, all, depth);
+        result.push(...found);
+    }
+
+    return result;
+}
+
+function search(root, key, value, settings = {}) {
+    if (key === undefined || value === undefined) {
+        printErr(`Object.search error: settings must have "key" and "value" props!`);
+        return;
+    }
+
+    const {
+        all,
+        children = 'children',
+        depth = 3
+    } = settings;
+
+    return this._searchInObject(root, key, value, children, all, depth);
+}
+
 export function define(obj, fields, options = {}) {
     const { enumer, config, write, get, set, value } = options;
 	let desc = {
