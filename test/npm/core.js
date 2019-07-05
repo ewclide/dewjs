4a8503396f93eec8d955ea64d7b2f56de7e52370
ewclide <ewclide@yandex.ru>
2019-07-06 02:36:49 +0300

    v3.0.0

--- test/npm/core.js
+++ test/npm/core.js
@@ -1,4 +1,9 @@
-import {html} from 'dewjs';
-import {randi} from 'dewjs/funcs';
+import { html } from 'dewjs';
+import { randi, log } from 'dewjs/funcs';
 
-console.log(randi)
+console.log(randi())
+
+log.warn('it works good!');
+
+const work = html.create('h1').text('It works good!');
+html.ready.then(() => html.body.append(work));
