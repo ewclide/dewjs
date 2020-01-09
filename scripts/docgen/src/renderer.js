eb4475c5fefa9c1dc852ecdc934bab51543597ce
ewclide <ewclide@yandex.ru>
2020-01-10 02:43:38 +0300

    updated

--- scripts/docgen/src/renderer.js
+++ scripts/docgen/src/renderer.js
@@ -61,7 +61,6 @@ const findTemplates = (() => {
 
             // @exp {. }
             .replace(/(\d+)\{(\:|\.)((?:\n|.)*?)\1\}/gm,
-                // (...a) => `${a[1]}{echo(\`${a[3]}\`,${a[2] === '.'});${a[1]}}`);
                 (...a) => `${a[1]}{${anchor(a[3], a[2] === '.')}${a[1]}}`);
 
         return { result, store };
@@ -69,22 +68,18 @@ const findTemplates = (() => {
 })();
 
 function prepareTemplates(store, str) {
-    const deanchor = (id, tpl) => {
-        return tpl.replace(//g, );
-    };
-
-    for (const { id, } of store) {
-        result = prepareOutputs(str);
-    }
+    return str.replace(/\<!(\d+)!\>/g, (...a) => {
+        const tpl = store.get(id);
+        return
+    });
 }
 
 function prepareOutputs(str) {
     return str
         .replace(/%(\d+)\{(.*?)\1\}/g, '\${$2}') // %{}
-        .replace(/%((?:[a-z]|\.)+)(\d+)\((.*?)\2\)/g, (a) => `\${${a.slice(1)}}`) // %exp()[]
-        // .replace(/%((?:\w|\.)+)(\d+)\((.*?)\2\)/g, (a) => '\${$1($3)}') // %func()
-        // .replace(/%((?:\w|\.)+)((\d+)\[\d+\2\])+/g, (a) => `\${${a.slice(1)}}`) // %elem[]
-        // .replace(/%((?:[a-z]|\.)+)([^a-z])/gi, '\${$1}$2') // %prop.prop
+        .replace(/%((?:\w|\.)+)(\d+)\((.*?)\2\)/g, (a) => '\${$1($3)}') // %func()
+        .replace(/%((?:\w|\.)+)((\d+)\[\d+\2\])+/g, (a) => `\${${a.slice(1)}}`) // %elem[]
+        .replace(/%((?:[a-z]|\.)+)([^a-z])/gi, '\${$1}$2') // %prop.prop
 }
 
 function prepareExpressions(str) {
@@ -108,6 +103,125 @@ function prepareSyntax(input) {
     tpl = removeBracketIndeces(tpl);
 
     return tpl;
+}
+
+class SyntaxParser {
+    constructor() {
+        this._tplId = 0;
+        this._tplStore = new Map();
+        this._brackets = ['()', '{}', '[]'];
+    }
+
+    _indexBrackets(str) {
+        const bracketStore = new Map();
+        const bracketTypes = [];
+
+        for (const bracket of this._brackets) {
+            const [open, close] = bracket.split('');
+            const data = { open, close, depth: 0 };
+
+            bracketStore.set(open, data);
+            bracketStore.set(close, data);
+            bracketTypes.push('\\' + open, '\\' + close);
+        }
+
+        const regexp = new RegExp(`(${bracketTypes.join('|')})`, 'gm');
+        let result = '';
+
+        return str.replace(regexp, (type) => {
+            const bracket = bracketStore.get(type);
+
+            if (type === bracket.close) bracket.depth--;
+            result = bracket.depth + type;
+            if (type === bracket.open) bracket.depth++;
+
+            return result;
+        });
+    }
+
+    _removeBracketIndeces(str) {
+        let result = str;
+
+        for (const bracket of this._brackets) {
+            const [open, close] = bracket.split('');
+            const regexp = new RegExp(`(\\d+)\\${open}((?:\\n|.)*?)\\1\\${close}`, 'gm');
+
+            result = result.replace(regexp, `${open}$2${close}`);
+        }
+
+        return result;
+    }
+
+    _anchor(tpl, trim) {
+        this._tplStore.set(this._tplId, trim ? tpl.trim() : tpl);
+        return `<!${this._tplId++}!>`;
+    };
+
+    _findTemplates(str) {
+        this._tplStore.clear();
+        this._tplId = 0;
+
+        return str
+            // @exp {. }
+            .replace(/(\d+)\{(\:|\.)((?:\n|.)*?)\1\}/gm,
+                (...a) => `${a[1]}{${this._anchor(a[3], a[2] === '.')}${a[1]}}`)
+
+            // %{ a ?. b : c}
+            .replace(/%(\d+)\{(.+?)\?(\:|\.)(.+?):(.+?)\1\}/gm,
+                (...a) => {
+                    const cond = a[2].trim();
+                    const trim = a[3] === '.';
+                    return `\${${cond}?${this._anchor(a[4], trim)}:${this._anchor(a[5], trim)}}`;
+                })
+    }
+
+    _prepareTemplates(str) {
+        if (!this._tplStore.size) return str;
+
+        return str.replace(/\<!(\d+)!\>/g, (...a) => {
+            const tpl = this._tplStore.get(+a[1]);
+            const parser = new SyntaxParser();
+
+            return `echo(\`${parser.prepare(tpl, false)}\`)`;
+        });
+    }
+
+    _prepareOutputs(str) {
+        return str
+            .replace(/%(\d+)\{(.*?)\1\}/g, '\${$2}') // %{}
+            .replace(/%((?:\w|\.)+)(\d+)\((.*?)\2\)/g, (a) => `\${${a.slice(1)}}`) // %func()
+            .replace(/%((?:\w|\.)+)((\d+)\[\d+\2\])+/g, (a) => `\${${a.slice(1)}}`) // %elem[]
+            .replace(/%((?:[a-z]|\.)+)([^a-z])/gi, '\${$1}$2') // %prop.prop
+    }
+
+    _prepareExpressions(str) {
+        return str.replace(/@(.*?)(\d+)\{((?:\n|.)+?)\2\}/gm,
+            (...tokens) => {
+                const [, expr,, body] = tokens;
+                return expr ? `<~${expr}{${body}}~>` : `<~${body}~>`
+            });
+    }
+
+    prepare(src, primary = true) {
+        let tpl = src;
+
+        if (primary) {
+            tpl = tpl.replace(/`/g, '\\`');
+            tpl = this._indexBrackets(tpl);
+        }
+
+        // console.log('prepapre:\n', tpl)
+        tpl = this._findTemplates(tpl); //console.log('findTpls:', tpl)
+        tpl = this._prepareOutputs(tpl); //console.log('outputs:', tpl)
+        tpl = this._prepareExpressions(tpl); //console.log('expr:', tpl)
+        tpl = this._prepareTemplates(tpl); console.log('prepTpls:\n', tpl, '\n')
+
+        if (primary) {
+           tpl = this._removeBracketIndeces(tpl); //console.log('remove:\n', tpl, '\n')
+        }
+
+        return tpl;
+    }
 }
 
 const $saved = Symbol('saved');
@@ -164,7 +278,8 @@ class Scope {
 
 function create(str, { vars, debug }) {
     const scope = new Scope();
-    const prep = prepareSyntax(str);
+    const prep = (new SyntaxParser).prepare(str);
+    console.log('prepared: ', prep)
     const tokens = prep
         .replace(/\<~/gm, '<~#')
         .split(/\<~|~\>/gm);
