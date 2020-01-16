e869b6bb084a2c8d56a21ef7f9e870aabb6d2c19
ewclide <ewclide@yandex.ru>
2020-01-16 23:45:28 +0300

    removed docgen to separeted package

--- scripts/docgen/test/index.js
+++
@@ -1,5 +0,0 @@
-// const path = require('path');
-const DocumentGenerator = require('../src');
-const docgen = new DocumentGenerator();
-
-docgen.genereate('./scripts/docgen/test/docs', './scripts/docgen/test/md');
